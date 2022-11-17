0320cde686e8cd91b2224b0a8c2a1c4be3beb22c
kcw0331 <changwoo03@naver.com>
2022-11-17 23:35:12 +0900

    2022년11월17일 - MSLicense 수업 후 챗봇 실습

---
+++ MS-License/chatbot/RootDialog.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Dialogs;
+using System.Net.Http;
+using GreatWall.Dialogs;
+using System.Collections.Generic;
+
+namespace GreatWall
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        string WelcomeMessage = "안녕하세요 만리장성 봇입니다. 1.주문 2.FAQ 중에 선택하세요";
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);  // 우리의 입력을 기다리고 MessageReceivedAsync로 넘어가게 된다.
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            await context.PostAsync(WelcomeMessage);
+
+            var message = context.MakeMessage();
+
+            var actions = new List<CardAction>();
+
+            actions.Add(new CardAction() { Title = "1.주문", Value = "1", Type = ActionTypes.ImBack });  // ImBack이 문자 입력기이다.
+            actions.Add(new CardAction() { Title = "2.FAQ", Value = "2", Type = ActionTypes.ImBack });
+
+
+            message.Attachments.Add(
+                new HeroCard
+                {
+                    Title = "원하는 기능을 선택하세요",
+                    Buttons = actions
+                }.ToAttachment()
+            );
+
+            await context.PostAsync(message);
+
+            context.Wait(SendWelcomeMessageAsync);
+        }
+
+        private async Task SendWelcomeMessageAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+            string selected = activity.Text.Trim(); // 앞 뒤의 공백을 제거해준다.
+
+            if (selected == "1")
+            {
+                await context.PostAsync("음식 주문 메뉴 입니다. 원하시는 음식을 입력해 주십시오.");
+                context.Call(new OrderDialog(), DialogResumeAfter); // context.call은 주문을 할 때 쓴다.
+            }
+            else if (selected == "2")
+            {
+                await context.PostAsync("FAQ 서비스 입니다. 질문을 입력해 주십시오.");
+                context.Call(new FAQDialog(), DialogResumeAfter);
+
+            }
+            else
+            {
+                await context.PostAsync("잘못 선택하셨습니다. 다시 선택해 주십시오");
+                context.Wait(SendWelcomeMessageAsync);
+            }
+
+        }
+
+        private async Task DialogResumeAfter(IDialogContext context, IAwaitable<string> result)
+        {
+            try
+            {
+                string message = await result;
+
+                await this.MessageReceivedAsync(context, result);
+            }
+            catch (TooManyAttemptsException)
+            {
+                await context.PostAsync("오류가 생겼습니다. 죄송합니다.");
+            }
+        }
+    }
+}
